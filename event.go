18b676ad7b98feaf9fe3289be1759481d05e7b85
Michael Gagliardo <mgagliardo91@gmail.com>
2019-01-24 16:10:46 -0500

    Updating event struct

--- event.go
+++ event.go
@@ -1,11 +1,38 @@
 package common
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // OfflineEvent represents an "event" such as a restaurant opening or festival
 type OfflineEvent struct {
-	Date    time.Time `json:"date"`
-	Title   string    `json:"title"`
-	Details string    `json:"details"`
-	URL     string    `json:"url"`
+	OfflineDate  time.Time `json:"offlineDate"`
+	OfflineURL   string    `json:"offlineUrl"`
+	ImageURL     string    `json:"imageUrl"`
+	Title        string    `json:"title"`
+	Teaser       string    `json:"teaser"`
+	Description  string    `json:"description"`
+	LocationRaw  string    `json:"locationRaw"`
+	DateRaw      string    `json:"dateRaw"`
+	TimeRaw      string    `json:"timeRaw"`
+	PriceRaw     string    `json:"priceRaw"`
+	EventURL     string    `json:"eventUrl"`
+	ReferralURLs []string  `json:"referralUrls"`
+	mux          *sync.Mutex
+}
+
+func NewOfflineEvent() *OfflineEvent {
+	return &OfflineEvent{
+		mux:          &sync.Mutex{},
+		ReferralURLs: make([]string, 0),
+	}
+}
+
+type UpdateFunc func()
+
+func (event *OfflineEvent) LockAndUpdate(update UpdateFunc) {
+	event.mux.Lock()
+	defer event.mux.Unlock()
+	update()
 }
